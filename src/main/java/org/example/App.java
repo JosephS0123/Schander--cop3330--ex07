46f4b2972eeedd203a960e751a76ad5969c8a428
Joseph <JosephSchander@gmail.com>
2021-09-10 12:58:06 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class App
+{
+
+    public static void main( String[] args )
+    {
+        double Constant = 0.09290304;
+        Scanner uInput = new Scanner(System.in);
+        System.out.println( "What is the length of the room in feet?" );
+        int Length = uInput.nextInt();
+        System.out.println( "What is the width of the room in feet?" );
+        int Width = uInput.nextInt();
+        System.out.println("You entered dimensions of "+ Length +" feet by " + Width+ " feet.\n The area is " +(Length*Width)+"square feet.\n"+(Constant*(Length*Width))+" square meters.");
+    }
+}
